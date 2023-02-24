ac883cc5cbc9721ce1a47d953c04bdde8ee476bd
ro-prg <ro.digraff@gmail.com>
2023-02-24 02:35:57 -0400

    split spread

--- code/fundamentals.js
+++ code/fundamentals.js
@@ -28,7 +28,13 @@ Assuming he's gonna grab a specific given number of bullets and move forward
 to fight another specific given number of dragons, will he survive?
 
 Return True if yes, False otherwise :) */
-
 function hero(bullets, dragons) {
   return bullets >= dragons * 2 ? true : false;
+}
+
+
+/* Reversed Strings (...=spread operator=>metodo para arrays) !=
+!= .split (metodo de string)*/
+function solution(str){
+  return [...str].reverse().join("")
 }
